f0c5fc18eca03fce51aff5d412ee4826dfa77d4c
Ashish Kumar <56781746+ashish620-boogle@users.noreply.github.com>
2022-08-11 15:58:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Outlet, Route, Routes } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+
+const Shop = () => {
+  return (
+    <div>
+      <div><h1>This is a Shop.</h1></div>
+      <Outlet/>
+    </div>
+  );
+}
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation/>} >
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop/>} />
+        <Route path='sign-in' element={<SignIn/>} />
+      </Route>
+    </Routes>
+  )
+};
+
+export default App;
